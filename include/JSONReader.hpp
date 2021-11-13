077a2e17bff3fba714d657c01777bd57ad548850
vmsou <73619111+vmsou@users.noreply.github.com>
2021-11-13 20:57:25 -0300

    JSON type detection. Hashable JSONElement header

--- include/JSONReader.hpp
+++ include/JSONReader.hpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <iostream>
 #include <sstream>
+#include <functional>
 #ifndef JSON_READER_HPP
 #define JSON_READER_HPP
 
@@ -11,36 +12,61 @@ struct BadJSONObject : std::exception {};
 struct JSONKeyNotFound : std::exception {};
 struct BadJSONConvert : std::exception {};
 
+enum class JSONType {
+	INVALID,
+	STRING,
+	NUMBER
+};
+
+
+struct JSONElement {
+	// Attributes
+	JSONType type;
+	std::string value;
+
+	bool operator==(const std::string& other) const { return value == other; }
+	bool operator==(const JSONElement& other) const { return value == other.value; }
+};
+
+std::istream& operator>>(std::istream& is, JSONElement& element);
+std::ostream& operator<<(std::ostream& os, const JSONElement& element);
+
 struct JSONPair {
 	// Attributes
-	std::string key;
-	std::string value;
+	JSONElement key;
+	JSONElement value;
 
 };
 
 // Stream operators
 std::istream& operator>>(std::istream& is, JSONPair& pair);
 
+namespace std {
+	template <>
+	struct hash<JSONElement> {
+		size_t operator()(const JSONElement& element) const {
+			return hash<string>()(element.value);
+		}
+	};
+}
+
 struct JSONObject {
 	// Attributes
-	std::unordered_map<std::string, std::string> map;
+	std::unordered_map<JSONElement, JSONElement> map;
 
 	// Methods
 	bool has(const std::string& key) const;
 
 	template <typename T>
 	T get(const std::string& key) const {
+		const JSONElement e{ JSONType::STRING, key };	// bad, fix this later
 		if (!has(key)) throw JSONKeyNotFound{};
-		std::istringstream iss{ map.at(key) };
+		std::istringstream iss{ map.at(e).value };
 		T converted;
 		if (!(iss >> converted)) throw BadJSONConvert{};
 
 		return converted;
 	}
-
-	// Access operators
-	std::string& operator[](const std::string& key) { return map.at(key); }
-	std::string operator[](const std::string& key) const { return map.at(key); }
 };
 
 // Stream operators
