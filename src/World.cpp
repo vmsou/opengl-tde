babdd3f5fdf61b3ac1e5e97e9ae3d1944e716bc5
vmsou <73619111+vmsou@users.noreply.github.com>
2021-11-13 21:00:05 -0300

    World Constructor: DisplayArray definition

--- src/World.cpp
+++ src/World.cpp
@@ -1,5 +1,14 @@
 #include "Object/World.hpp"
 
+// Constructors
+World::World(const DisplayArray& dArr) {
+	for (const DisplayFile& df : dArr) {
+		if (df.type == 1) this->add(Disc::create(df));
+		else if (df.type == 2) this->add(Cone::create(df));
+
+	}
+}
+
 // Methods
 void World::add(Object3D& object) { objects.push_back(&object); }
 
